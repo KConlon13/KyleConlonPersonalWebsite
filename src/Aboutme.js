8fa2be602eb2e8a1b0347023121a3fb757664e09
KConlon13 <KyleConlon13@gmail.com>
2020-01-20 15:53:19 -0500

    added language/skills section and colorized all icons for each and every list item from font-awesome. still need to edit sizing and general style but the overall idea is present

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import KyleDrumming from "./KyleDrumming.jpg"
+import KyleDrumming from "./KyleDrumming2.jpg"
 import { Link } from "react-router-dom";
 
 class Aboutme extends React.Component {
@@ -31,7 +31,7 @@ class Aboutme extends React.Component {
 
             <div className="BioBoxImage">
               <img className="ui medium rounded image" src={KyleDrumming} alt="Kyle Drumming" 
-              style={{minHeight: 260, minWidth: 480, marginRight: "5vh"}}/>
+              style={{minHeight: 260, minWidth: 480, marginRight: "5vh", borderRadius: "0.2em"}}/>
             </div>
           </div>
           {/* End of Bio Box on Top */}
@@ -39,8 +39,73 @@ class Aboutme extends React.Component {
           <div className="whatIDo">
             <h2 style={{textAlign: "left", paddingLeft: "5vw", fontSize: "29px"}}>What I do</h2>
           </div>
-          <div>
+          <div className="SkillsGrid">
             {/* List Here in a grid all of the languages broken down by section with logos */}
+
+            <div className="ui middle aligned list" style={{paddingTop: "1em"}}>
+                <div class="item">
+                  <div class="content">
+                    <u><div class="header">Front-End Skills</div></u>
+                      <div class="item" style={{paddingBottom: "5px", paddingTop: "5px"}}>
+                      <i class="fab fa-js-square" style={{color: "rgb(250, 242, 91)", paddingRight: "4px"}}></i>Javascript</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fab fa-react" style={{color: "rgb(20, 228, 228)", paddingRight: "4px"}}></i>
+                      React.js</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                        <i class="fab fa-react" style={{color: "rgb(20, 228, 228)", paddingRight: "4px"}}></i>React Native</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fab fa-html5" style={{color: "orange", paddingRight: "4px"}}></i>
+                        HTML</div>
+                      <div class="item">
+                        <i class="fab fa-css3-alt" style={{color: "#2965f1", paddingRight: "4px"}}></i>
+                        CSS</div>
+                  </div>
+                </div>
+            </div>
+
+            <div className="ui middle aligned list">
+                <div class="item">
+                  <div class="content">
+                    <u><div class="header">Back-End Skills</div></u>
+                      <div class="item" style={{paddingBottom: "5px", paddingTop: "5px"}}>
+                        <i class="far fa-gem" style={{color: "red", paddingRight: "4px"}}></i>Ruby</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fas fa-gem" style={{color: "red", paddingRight: "4px"}}></i>Rails</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fas fa-database" style={{color: "grey", paddingRight: "4px"}}></i>SQL</div>
+                  </div>
+                </div>
+            </div>
+
+            <div className="ui middle aligned list">
+                <div class="item">
+                  <div class="content">
+                    <u><div class="header">Other Skills</div></u>
+                      <div class="item" style={{paddingBottom: "5px", paddingTop: "5px"}}>
+                      <i class="fab fa-github" style={{color: "black", paddingRight: "4px"}}></i>Git</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fas fa-map-marker-alt" style={{color: "purple", paddingRight: "4px"}}></i>
+                        Heroku</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fab fa-bootstrap" style={{color: "purple", paddingRight: "4px"}}></i>
+                        Bootstrap UI</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fab fa-stripe-s" style={{color: "rgb(20, 228, 228)", paddingRight: "4px"}}></i>
+                        Semantic UI</div>
+                      <div class="item" style={{paddingBottom: "5px"}}>
+                      <i class="fab fa-react" style={{color: "rgb(18, 198, 200)", paddingRight: "4px"}}></i>
+                        React Native Elements</div>
+                  </div>
+                </div>
+            </div>
+
+            {/* <div className="backEndSkills" style={{display: "inline"}}>
+              <h3>Back-End Skills</h3>
+            </div>
+            <div className="otherSkills" style={{display: "inline"}}>
+              <h3>Other Skills</h3>
+            </div> */}
+
           </div>
 
 
