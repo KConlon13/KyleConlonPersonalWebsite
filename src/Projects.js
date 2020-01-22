bbe303a0299977286bc47316a6cfb5ce07a3a6b1
KConlon13 <KyleConlon13@gmail.com>
2020-01-22 12:29:38 -0500

    added project headers and images to projects component

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import ArtWorkImage from "./ArtWork.png"
+import ChewsyImage from "./Chewsy.png"
+import PotterlessImage from "./Potterless.png"
 
 class Projects extends React.Component {
 
   render(){
     return (
         <div>
+
           <div class="PortfolioGrayHeaderBox">
             <h1 style={{fontSize: "31px", marginBottom: "1.2vh"}}>Portfolio</h1>
             <h4 style={{fontSize: "16px", fontWeight: "100", marginTop: 0, paddingTop: 0, paddingLeft: "10.5vw", paddingRight: "10.5vw", lineHeight: "3.3vh", display: "block", marginBottom: "2vh"}}>Welcome to my online portfolio. Feel free to look through my most recent projects, view their demo videos on YouTube, or examine my code on Github. </h4>
@@ -17,9 +21,26 @@ class Projects extends React.Component {
                   </button>
               </a>
             </div>
-
           </div>  
-            <h5>Projects Here</h5>
+
+          <div class="projectShowContainer">
+
+            <div class="projectCard">
+              <h1 class="projectCardHeader">ArtWork</h1>
+              <img class="projectImage" src={ArtWorkImage} alt="ArtWork"/>
+            </div>
+
+            <div class="projectCard">
+              <h1 class="ChewsyCardHeader">Chewsy</h1>
+              <img class="ChewsyImage" src={ChewsyImage} alt="Chewsy"/>
+            </div>
+
+            <div class="projectCard">
+              <h1 class="projectCardHeader">Potterless</h1>
+              <img class="projectImage" src={PotterlessImage} alt="Potterless"/>
+            </div>
+          </div>
+
         </div>
     );
   }
