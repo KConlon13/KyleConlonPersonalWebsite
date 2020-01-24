5f22c5e01fa76a1e78a941001c257268963bc38c
KConlon13 <KyleConlon13@gmail.com>
2020-01-24 17:59:23 -0500

    imported bootstrap and styles along with it

--- src/Resume.js
+++ src/Resume.js
@@ -10,7 +10,7 @@ class Resume extends React.Component {
           <div class="ResumeGrayHeaderBox">
             <h1 style={{fontSize: "31px", marginBottom: "2vh"}}>Online Resume</h1>
 
-            <button class="ui button" style={{background: "rgb(15, 165, 157)", color: "white", width: "17vw"}}>
+            <button class="ui button" style={{background: "rgb(15, 165, 157)", color: "white", width: "17vw"}}onClick={()=>this.buttonHandler()} >
               <i class="fas fa-file-pdf fa-lg" style={{display: "inline", verticalAlign: "25%"}}></i>
               <h3 style={{display: "inline", marginLeft: "1vw", fontSize: "15px", verticalAlign: "25%"}}>Download PDF Version</h3>
             </button>
