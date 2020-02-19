2b0a1ac35acbd4570f6a5aa0f7d60f4e6964ddf9
KConlon13 <KyleConlon13@gmail.com>
2020-02-19 13:25:58 -0500

    solved PDF download bug by using Google Drive

--- src/Resume.js
+++ src/Resume.js
@@ -10,11 +10,13 @@ class Resume extends React.Component {
           <div class="ResumeGrayHeaderBox">
             <h1 style={{fontSize: "31px", marginBottom: "2vh"}}>Online Resume</h1>
 
-            <button class="ui button" style={{background: "rgb(15, 165, 157)", color: "white", width: "17vw"}}onClick={()=>this.buttonHandler()} >
+          <a href="https://drive.google.com/file/d/10mo1KIuPAXXVeWioiydJY6abmwzIU_hR/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <button class="ui button" style={{background: "rgb(15, 165, 157)", color: "white", width: "17vw"}}>
               <i class="fas fa-file-pdf fa-lg" style={{display: "inline", verticalAlign: "25%"}}></i>
               <h3 style={{display: "inline", marginLeft: "1vw", fontSize: "15px", verticalAlign: "25%"}}>Download PDF Version</h3>
             </button>
-            
+          </a>  
+          
           </div>
             <img src={ResumeImage} 
             alt="Kyle's Resume" 
